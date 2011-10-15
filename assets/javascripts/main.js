e71d1c661de9083eccbcb72761fde47a52ad9ebb
Sergio Ramirez <sirtdevelopment@gmail.com>
2011-10-16 00:41:10 +0200

    Page almost done

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -9,32 +9,37 @@ $(document).ready(function(){
       +" </span><span class='label'>("+key+")</span><br/>");
   }
   
-  $.responsive()
-    .from(0,480,"mobile")
-      .enter(log)
-      .enter(function(){
-        $(".gist_container, .console").css({
-          width:"100%", float:"none",
-          "margin-left":0, "margin-right":0
-        })
-        $("h1").css("font-size","39px")
-      })
-    .from(480,768,"tablet")
-      .enter(log)
-      .enter(function(){
-        $(".gist_container, .console").css({
-          width:"48%", float:"left"
-        });
-        $(".gist_container").css({
-          "margin-left":"2%"
-        });
-        $(".console").css({
-          "margin-right":"2%"
-        });
-        $("h1").css("font-size","69px")
-      })
-    .from(768,992,"default")
-      .enter(log)
-      .exit(log)
-    .start();
+function mobile_enter(){
+  $(".gist_container, .console").css({
+    width:"100%", float:"none",
+    "margin-left":0, "margin-right":0
+  })
+}
+
+$.responsive()
+  .from(0,480,"mobile")
+    .enter(log)
+    .enter(mobile_enter)
+    .enter(function(){
+      $("h1").css("font-size","39px")
+    })
+  .from(480,768,"tablet")
+    .enter(log)
+    .enter(mobile_enter)
+  .from(768,992,"default")
+    .enter(log)
+    .exit(log)
+    .enter(function(){
+      $(".gist_container, .console").css({
+        width:"48%", float:"left"
+      });
+      $(".gist_container").css({
+        "margin-left":"2%"
+      });
+      $(".console").css({
+        "margin-right":"2%"
+      });
+      $("h1").css("font-size","69px")
+    })
+  .start();
 })
